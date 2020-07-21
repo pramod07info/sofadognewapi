1426406d78dc57922ea274c4199bfa5bcdc33980
pramod07info <pramod07info@gmail.com>
2020-07-22 00:02:18 +0530

    Fetch All Feeds

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,20 @@ import { title } from 'process'
 const prisma = new PrismaClient()
 const app = express()
 
+interface IFeed{
+  id: any,
+  enqueued: any,
+  ordinal: any,
+  url: any,
+  published: any,
+  categories: any,
+  credits: any,
+  title: any
+}
+
 app.use(bodyParser.json())
 
 app.post(`/categories`, async (req, res) => {
-
   const result = await prisma.categories.create({
     data: {
       ...req.body,
@@ -21,14 +31,61 @@ app.post(`/categories`, async (req, res) => {
 
 app.get('/categories', async (req, res) => {
   const posts = await prisma.categories.findMany({
+    where:{
+      is_active:true
+    }, 
     select:{
       id:true,
       color:true,
       title:true
     }
-    
   })
   res.json(posts)
+})
+
+app.get('/refresh', async (req, res) => {
+  const posts = await prisma.feeds.findMany({
+    where:{
+      published:true
+    },
+    select:{
+      id:true,
+      enqueued:true,
+      ordinal:true,
+      url:true,
+      published:true,
+      categories:{
+        select:{
+          id:true
+        }
+      },
+      credits:true,
+      title:true
+    }
+   
+  })
+ 
+  let response:IFeed[] = [];
+  
+  posts.forEach(function(data){
+    let cat= [];
+    if(data.categories != null){
+      cat.push(data.categories.id.toString());
+    }
+    var result:IFeed = {
+      id: data.id,
+      enqueued: data.enqueued,
+      ordinal: data.ordinal,
+      url: data.url,
+      published: data.published,
+      categories:cat,
+      credits: data.credits,
+      title: data.title
+    }
+    response.push(result)
+  });
+
+  res.json(response)
 })
 
 const server = app.listen(3000, () =>
