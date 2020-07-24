1520c84da06ef06338fc8dca2ed55f448f3a64da
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-07-24 10:09:48 +0530

    Save refresh log

---
+++ src/model/refresh_log.ts
@@ -0,0 +1,9 @@
+export interface IRefreshLog{
+    uuid: string;
+    height: number;
+    os: string;
+    from?: string;
+    manufacture?: string;
+    model?: string;
+    width?: number;
+}
--- src/repositories/feed_repository.ts
+++ src/repositories/feed_repository.ts
@@ -1,60 +1,79 @@
 import { PrismaClient } from '@prisma/client'
+import { IRefreshLog } from '../model'
 
 const prisma = new PrismaClient()
 
-export class FeedRepository{
+export class FeedRepository {
     async get(req: any) {
+        this.save_refresh_log(req);
         const result = await prisma.feeds.findMany({
-            where:{
-                published:true
+            where: {
+                published: true
             },
-            select:{
-                id:true,
-                enqueued:true,
-                ordinal:true,
-                url:true,
-                published:true,
-                categories:{
-                    select:{
-                        id:true
+            select: {
+                id: true,
+                enqueued: true,
+                ordinal: true,
+                url: true,
+                published: true,
+                categories: {
+                    select: {
+                        id: true
                     }
                 },
-                credits:true,
-                title:true
+                credits: true,
+                title: true
             },
-            orderBy:{
-                ordinal:'desc'
-            } 
+            orderBy: {
+                ordinal: 'desc'
+            }
         })
         return result
     }
 
     async get_gt_ordinal(req: any) {
+        this.save_refresh_log(req);
         const result = await prisma.feeds.findMany({
-            where:{
-                published:true,
-                ordinal:{
+            where: {
+                published: true,
+                ordinal: {
                     gt: parseInt(req.params.ordinal)
                 },
             },
-            select:{
-                id:true,
-                enqueued:true,
-                ordinal:true,
-                url:true,
-                published:true,
-                categories:{
-                    select:{
-                        id:true
+            select: {
+                id: true,
+                enqueued: true,
+                ordinal: true,
+                url: true,
+                published: true,
+                categories: {
+                    select: {
+                        id: true
                     }
                 },
-                credits:true,
-                title:true
+                credits: true,
+                title: true
             },
-            orderBy:{
-                ordinal:'desc'
-            }  
+            orderBy: {
+                ordinal: 'desc'
+            }
         })
         return result
+    }
+
+    async save_refresh_log(req: any) {
+        var refresh_log: IRefreshLog = {
+            uuid: req.params.uuid,
+            height: req.params.height,
+            os: req.params.os
+        }
+
+        const result = await prisma.refresh_log.create({
+            data: {
+                uuid: req.params.uuid,
+                height: Number(req.params.height),
+                os: req.params.os
+            },
+        })
     }
 }
